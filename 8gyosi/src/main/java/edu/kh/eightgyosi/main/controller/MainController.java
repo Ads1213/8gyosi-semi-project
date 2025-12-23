a8bfa701479c78f3340a93d27940d6aaafef0604
binlduu25 <binlduu25@gmail.com>
2025-12-23 12:26:25 +0900

    25.12.13. 초기설정버전

---
+++ 8gyosi/src/main/java/edu/kh/eightgyosi/main/controller/MainController.java
@@ -0,0 +1,18 @@
+package edu.kh.eightgyosi.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/")
+	public String mainPage() {
+		return "common/main"; // forward
+	}
+	
+	
+	
+}
