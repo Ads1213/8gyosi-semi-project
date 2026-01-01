0a616e9e91700ef1446d8b3bce6cd12b66c24563
Ads1213 <ads11121316@gmail.com>
2026-01-01 13:51:51 +0900

    쿠키 작업 중

--- 8gyosi/src/main/java/edu/kh/eightgyosi/member/model/service/MemberServiceImpl.java
+++ 8gyosi/src/main/java/edu/kh/eightgyosi/member/model/service/MemberServiceImpl.java
@@ -1,11 +1,11 @@
-package edu.kh.eightgyosi.model.service;
+package edu.kh.eightgyosi.member.model.service;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import edu.kh.eightgyosi.model.dto.Member;
-import edu.kh.eightgyosi.model.mapper.MemberMapper;
+import edu.kh.eightgyosi.member.model.dto.Member;
+import edu.kh.eightgyosi.member.model.mapper.MemberMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -34,7 +34,22 @@ public class MemberServiceImpl implements MemberService {
 		// 1. 이메일이 일치하면서 탈퇴하지 않은 회원의 ( + 비밀번호 ) 조회
 		Member loginMember = mapper.login(member.getMemberEmail());
 		
-		return null;
+		// 2. 만약에 일치하는 이메일이 없어서 조회 결과가 null 인 경우
+		if(loginMember == null) return null;
+		
+		// 3. 입력받은 비밀번호(평문 : member.getMemberPw()) 와
+		// 	  암호화된 비밀번호(loginMember.getMemberPw())
+		//	  두 비밀번호가 일치하는지 확인
+		
+		// bcrypt.matches(평문, 암호화) : 평문과 암호화가 내부적으로
+		//					일치한다고 판단이 되면 true , 아니면 false
+		if(!bcrypt.matches(member.getMemberPw(), 
+						loginMember.getMemberPw())) return null;
+		
+		// 로그인한 회원 정보에서 비밀번호 제거
+		loginMember.setMemberPw(null);
+		
+		return loginMember;
 	}
 	
 	
