e6955aaaee45b2c428d179a47efd47b585391a64
binlduu25 <binlduu25@gmail.com>
2026-01-02 16:55:56 +0900

    철구님 포함

--- 8gyosi/src/main/java/edu/kh/eightgyosi/common/config/WebConfig.java
+++ 8gyosi/src/main/java/edu/kh/eightgyosi/common/config/WebConfig.java
@@ -1,18 +1,19 @@
+package edu.kh.eightgyosi.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
 /*
- * package edu.kh.eightgyosi.common.config;
- * 
- * import org.springframework.context.annotation.Configuration; import
- * org.springframework.web.servlet.config.annotation.*;
- * 
- * import lombok.RequiredArgsConstructor;
- * 
- * @Configuration
- * 
- * @RequiredArgsConstructor public class WebConfig implements WebMvcConfigurer {
- * 
- * private final FileConfig fileConfig;
- * 
- * @Override public void addResourceHandlers(ResourceHandlerRegistry registry) {
- * registry.addResourceHandler(fileConfig.getWebPath() + "**")
- * .addResourceLocations("file:" + fileConfig.getUploadPath()); } }
- */
+    private final FileConfig fileConfig;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(fileConfig.getWebPath() + "**")
+                .addResourceLocations("file:" + fileConfig.getUploadPath());
+    }*/
+}
