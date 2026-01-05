7000fd54302f21c053d276e035b4a74135a6e3d9
huni1905-droid <huni1905@gmail.com>
2026-01-05 16:46:14 +0900

    채팅창 구성중 중간 병합용

--- 8gyosi/src/main/java/edu/kh/eightgyosi/chatting/controller/ChattingController.java
+++ 8gyosi/src/main/java/edu/kh/eightgyosi/chatting/controller/ChattingController.java
@@ -1,12 +1,21 @@
 package edu.kh.eightgyosi.chatting.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
+import edu.kh.eightgyosi.chatting.model.dto.ChattingRoom;
 import edu.kh.eightgyosi.chatting.model.service.ChattingService;
-import edu.kh.eightgyosi.chatting.model.service.ChattingServiceImpl;
+import edu.kh.eightgyosi.member.model.dto.Member;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
@@ -18,7 +27,36 @@ public class ChattingController {
 	private ChattingService service;
 	
 	@GetMapping("")
-	public String chatting() {
+	public String chat() {
 		return "chatting/chatting";
 	}
+	
+/*	
+	@GetMapping("list")
+	public String chatting(@SessionAttribute("loginMember") Member loginMember,
+						Model model
+			) {
+
+		List<ChattingRoom> roomList = service.selectRoomList(loginMember.getMemberNo());
+		
+		model.addAttribute("roomList", roomList);
+		
+		return "chatting/chatting";
+	}
+	// 채팅 상대 검색 - 비동기 
+	@GetMapping("selectTarget")
+	@ResponseBody // 비동기 요청이니깐
+	public List<Member> selectTarget(@RequestParam("query") String query, 
+									// queryString 형태로 받아왔다면 @RequestParam으로 갖고와야 한다
+									@SessionAttribute("loginMember") Member loginMember
+									) {
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("memberNo", loginMember.getMemberNo());
+		map.put("query", query);
+		
+		return service.selectTarget(map);
+		
+	}
+ */
 }
--- 8gyosi/src/main/java/edu/kh/eightgyosi/chatting/model/mapper/ChattingMapper.java
+++ 8gyosi/src/main/java/edu/kh/eightgyosi/chatting/model/mapper/ChattingMapper.java
@@ -1,8 +1,29 @@
 package edu.kh.eightgyosi.chatting.model.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import edu.kh.eightgyosi.chatting.model.dto.ChattingRoom;
+import edu.kh.eightgyosi.member.model.dto.Member;
 
 @Mapper
 public interface ChattingMapper {
+
+	/** 채팅방 검색 SQL
+	 * @param memberNo
+	 * @return
+	 */
+	List<ChattingRoom> selectRoomList(int memberNo);
+
+	/** 채팅상대 검색 SQL 
+	 * @param map
+	 * @return
+	 */
+	List<Member> selectTarget(Map<String, Object> map);
+	
+	
 
 }
