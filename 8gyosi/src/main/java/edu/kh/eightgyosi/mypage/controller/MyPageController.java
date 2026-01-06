bb30468716c084c9eab12e0cd659e34659157ac5
Ads1213 <ads11121316@gmail.com>
2026-01-06 09:10:21 +0900

    회원 주소 변경 구현 중

--- 8gyosi/src/main/java/edu/kh/eightgyosi/mypage/controller/MyPageController.java
+++ 8gyosi/src/main/java/edu/kh/eightgyosi/mypage/controller/MyPageController.java
@@ -125,10 +125,33 @@ public class MyPageController {
 	
 	/** 마이페이지 프로필 수정 화면으로 이동
 	 * @author dasol
+	 * @param loginMember
 	 * @return
 	 */
 	@GetMapping("info")
-	public String myPageInfo() {
+	public String info(@SessionAttribute("loginMember") Member loginMember,
+							Model model) {
+		
+		// 현재 로그인한 회원의 주소를 꺼내옴
+		// 현재 로그인한 회원 정보 -> session scope 에 등록된 상태(loginMember)
+		// loginMember(memberAddress도 포함)
+		// -> 만약 회원가입 당시 주소를 입력했다면 주소값 문자열(^^^ 구분자로 만들어진 문자열)
+		// -> 	   회원가입 당시 주소를 입력하지 않았다면 null
+		String memberAddress = loginMember.getMemberAddress();
+		// 03189^^^서울 종로구 우정국로2길 21^^^3층, 302클래스 (대왕빌딩)
+		// or null
+		
+		if(memberAddress != null) { // 주소가 있을 경우에만 동작
+			// 구분자 "^^^" 를 기준으로
+			// memberAddress 값을 쪼개어 String[] 로 반환
+			String[] arr = memberAddress.split("\\^\\^\\^");
+			log.info("memberAddress" + memberAddress);
+			// ["03189", "서울 종로구 우정국로2길 21", "3층, 302호 클래스 (대왕빌딩)"]
+			
+			model.addAttribute("postcode", arr[0]); // 우편주소
+			model.addAttribute("address", arr[1]); // 도로명/지번주소
+			model.addAttribute("detailAddress", arr[2]); // 상세주소
+		}
 		
 		return "myPage/myPage-info";
 	}
@@ -156,11 +179,6 @@ public class MyPageController {
 	public String fileList() {
 		return "/myPage/fileList";
 	}
-	
-	
-	
-	
-	
 	
 	
 }
