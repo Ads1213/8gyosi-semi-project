206b796e20c6303b41372d672e749fff7a340608
Ads1213 <ads11121316@gmail.com>
2026-01-09 02:54:13 +0900

    내 정보 변경 닉네임 중복 검사 완료

--- 8gyosi/src/main/java/edu/kh/eightgyosi/mypage/controller/MyPageController.java
+++ 8gyosi/src/main/java/edu/kh/eightgyosi/mypage/controller/MyPageController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -406,30 +407,31 @@ public class MyPageController {
 	
 
 	
-	@PostMapping("diary/selectDiary")
-	public DiaryDTO selectDiary(@SessionAttribute("loginMember") Member loginMember,
-	                            @RequestBody DiaryDTO inputDiary) { 
-		log.info("넘어온 날짜: " + inputDiary.getDiaryDate());
-		
-		int memberNo = loginMember.getMemberNo();
-		String inputDiaryDate = inputDiary.getDiaryDate().strip(); // **** 트러블 슈팅에 이용
-		inputDiary.setDiaryDate(inputDiaryDate);
-	    inputDiary.setMemberNo(loginMember.getMemberNo());
-	    
-	    DiaryDTO result = diaryService.selectDiary(inputDiary);
-	    
-	    // 1. 서비스 호출 (결과를 DTO 객체로 받음)
-	    DiaryDTO diary = diaryService.selectDiary(inputDiary);
-	    
-	    if(diary != null) {
-
-	        model.addAttribute("diary", diary);
-	        return "redirect:/myPage";  
-	    } else {
-	        ra.addFlashAttribute("message", "해당 날짜에 작성된 일기가 없습니다.");
-	        return "redirect:/myPage"; 
-	    }
-	}
+//	@PostMapping("diary/selectDiary")
+//	public DiaryDTO selectDiary(@SessionAttribute("loginMember") Member loginMember,
+//	                            @RequestBody DiaryDTO inputDiary
+//	                            ) { 
+//		log.info("넘어온 날짜: " + inputDiary.getDiaryDate());
+//		
+//		int memberNo = loginMember.getMemberNo();
+//		String inputDiaryDate = inputDiary.getDiaryDate().strip(); // **** 트러블 슈팅에 이용
+//		inputDiary.setDiaryDate(inputDiaryDate);
+//	    inputDiary.setMemberNo(loginMember.getMemberNo());
+//	    
+//	    DiaryDTO result = diaryService.selectDiary(inputDiary);
+//	    
+//	    // 1. 서비스 호출 (결과를 DTO 객체로 받음)
+//	    DiaryDTO diary = diaryService.selectDiary(inputDiary);
+//	    
+//	    if(diary != null) {
+//
+////	        model.addAttribute("diary", diary);
+////	        return "redirect:/myPage";
+//	    } else {
+//	        ra.addFlashAttribute("message", "해당 날짜에 작성된 일기가 없습니다.");
+//	        return "redirect:/myPage"; 
+//	    }
+//	}
 
 
 	/** 일기 내용 삭제
@@ -525,21 +527,15 @@ public class MyPageController {
 		String message = null;
 
 		if (result > 0) {
-			message = "회원 정보 수정 성공!!!";
-
-			// member에 DB상 업데이트된 내용으로 세팅
-			// -> member는 세션에 저장된 로그인한 회원 정보가
-			// 저장되어 있다 (로그인 할 당시의 기존 데이터)
-			// -> member를 수정하면 세션에 저장된 로그인한 회원의
-			// 정보가 업데이트 된다
-			// == Session에 있는 회원 정보와 DB 데이터를 동기화
+			message = "회원 정보 수정 성공";
 
 			loginMember.setMemberNickname(member.getMemberNickname());
 			loginMember.setMemberTel(member.getMemberTel());
 			loginMember.setMemberAddress(member.getMemberAddress());
 
 		} else {
-			message = "회원 정보 수정 실패...";
+			
+			message = "회원 정보 수정 실패";
 
 		}
 
@@ -631,101 +627,15 @@ public class MyPageController {
 		return "redirect:" + path;
 	}
 
-	// 파일 테스트 화면으로 이동
-	@GetMapping("fileTest")
-	public String fileTest() {
-		
-		return "myPage/myPage-fileTest";
-	}
 	
-	/*
-	 * Spring에서 파일을 처리하는 방법
-	 * 
-	 * - enctype="multipart/form-data"로 클라이언트의 요청을 받으면
-	 * 	 (문자, 숫자, 파일 등이 섞여있는 요청)
-	 * 		
-	 * 	 이를 MultipartResolver(FileConfig에 정의)를 이용해서
-	 * 	 섞여있는 파라미터를 분리 작업을 함
-	 * 		
-	 * 	 문자열, 숫자 -> String
-	 * 	 파일		  -> MultipartFile
-	 *   	 
-	 * 
-	 * */
-	@PostMapping("file/test1") // /myPage/file/test1
-	public String fileUpload1(@RequestParam("uploadFile") MultipartFile uploadFile,
-								RedirectAttributes ra) {
-		
-		try {
-			String path = myPageService.fileUpload1(uploadFile);
-				// /myPage/file/파일명.jpg
-			
-			// 파일이 실제로 서버 컴퓨터에 저장이 되어
-			// 웹에서 접근할 수 있는 경로가 반환되었을 때
-			if(path != null) {
-				ra.addFlashAttribute("path", path);
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-			log.info("파일 업로드 예제1 중 예외 발생");
-		}
-		
-		return "redirect:/myPage/fileTest";
-	}
-	
-// seongjong
-	
-
-	
-	
-
-	
-
-	//	// 파일 테스트 화면으로 이동
-	//	@GetMapping("fileTest")
-	//	public String fileTest() {
-	//		return "myPage/myPage-fileTest";
-	//	}
-	
-	@PostMapping("file/test2") // /myPage/file/test2
-	public String fileUpload2(@RequestParam("uploadFile") MultipartFile uploadFile,
-							@SessionAttribute("loginMember") Member loginMember,
-							RedirectAttributes ra) {
-		
-		try {
-			
-			// 로그인한 회원의 번호 얻어오기 (누가 업로드 했는가)
-			int memberNo = loginMember.getMemberNo();
-			
-			// 업로드된 파일 정보를 DB에 INSERT 후 결과 행의 갯수 반환 받아옴
-			int result = myPageService.fileUpload2(uploadFile, memberNo);
-			
-			String message = null;
-			
-			if(result > 0) {
-				message = "파일 업로드 성공";
-			} else {
-				message = "파일 업로드 실패";
-			}
-			
-			ra.addFlashAttribute("message", message);
-			
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-			log.info("파일 업로드 테스트2 중 예외발생");
-		}
-		
-		return "redirect:/myPage/fileTest";
-	}
-	
+	// 회원 프로필 변경 페이지로 이동
 	@GetMapping("profile") // /myPage/profile
 	public String profile() {
 		
 		return "myPage/myPage-profile";
 	}
 	
+	// 회원 프로필 변경
 	@PostMapping("profile") // /myPage/profile
 	public String profile(@RequestParam("profileImg") MultipartFile profileImg,
 						@SessionAttribute("loginMember") Member loginMember,
@@ -745,12 +655,6 @@ public class MyPageController {
 		ra.addFlashAttribute("message", message);
 		
 		return "redirect:profile"; // 리다이렉트 - /myPage/profile GET 요청
-	}
-	
-	// 파일 목록 조회 화면 이동
-	@GetMapping("fileList")
-	public String fileList() {
-		return "/myPage/fileList";
 	}
 	
 	
